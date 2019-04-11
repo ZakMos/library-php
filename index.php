4118f26a9de2ddeb8229ab3e063e9a13ed494146
ZakMos <z.zza@hotmail.com>
2019-04-11 14:02:58 +0200

    initial commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Library Home Page</title>
+  </head>
+  <body>
+    <?php
+
+
+     ?>
+     <div class="">
+       <h3>Welcome to the Library!</h3>
+       <a href="booksearch.html">Browse our books</a>
+     </div>
+  </body>
+</html>
